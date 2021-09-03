5c734772fff6c6d513c8b4861d84fe25ecf30634
GabrielRossin <gabrielfiliperossin@hotmail.com>
2021-09-03 19:56:47 -0300

    Construtores e Membros estáticos
    Construtor da classe, possibilitando receber argumentos e inicializar atributos a partir da criação de um objeto
    Com isso, a inicialização dos atributos recebidos no construtor torna-se obrigatória
    Atributos da classe, os atributos estáticos
    Métodos da classe, os métodos estáticos
    Ausência de referência, do this, dentro de métodos estáticos

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -5,7 +5,17 @@ public class Conta {
 	private double saldo;
     private int agencia;
     private int numero;
-    private Cliente titular = new Cliente();
+    private Cliente titular;
+    private static int total;
+    
+    public Conta(int agencia, int numero) 
+    { 
+    	 Conta.total ++;
+         System.out.println("o total de contas é " + Conta.total);
+         this.agencia = agencia; 
+         this.numero = numero; 
+         System.out.println("estou criando uma conta " + this.numero); 
+    } 
 	
     public void deposita(double valor)
     {
@@ -51,7 +61,12 @@ public class Conta {
     
     public void setNumero(int numero) 
     { 
-        this.numero = numero; 
+    	if (numero <= 0) 
+		{
+            System.out.println("não pode valor <= 0");
+            return;
+        }
+    	this.numero = numero; 
     }
     
     public int getAgencia() {
@@ -60,7 +75,11 @@ public class Conta {
     
     public void setAgencia(int agencia) 
     {
-		this.agencia = agencia;
+    	if (agencia <= 0) { 
+            System.out.println("nao pode valor menor igual a 0");
+            return;
+        }
+    	this.agencia = agencia;
 	}
     
     public Cliente getTitular() 
@@ -72,5 +91,11 @@ public class Conta {
     {
 		this.titular = titular;
 	}
+    
+   
+    
+    public static int getTotal() { 
+        return Conta.total;  
+    }
 
 }
